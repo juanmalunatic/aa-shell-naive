a3be2fc175e99774130679dc7be4f51780e0f472
Juan Manuel Luna López <lunalopezjm@gmail.com>
2020-06-29 22:05:20 -0500

    Add JointJs basics to AA component

--- src/app/auto-attendant/auto-attendant.component.ts
+++ src/app/auto-attendant/auto-attendant.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as joint from 'jointjs/dist/joint.js';
+
 @Component({
   selector: 'app-auto-attendant',
   templateUrl: './auto-attendant.component.html',
-  styleUrls: ['./auto-attendant.component.css']
+  styleUrls: ['./auto-attendant.component.css'],
 })
 export class AutoAttendantComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.createGraph();
   }
 
+  private createGraph() {
+    var graph = new joint.dia.Graph();
+
+    var paper = new joint.dia.Paper({
+      el: document.getElementById('myholder'),
+      model: graph,
+      width: 600,
+      height: 200,
+      gridSize: 1,
+    });
+
+    var rect = new joint.shapes.standard.Rectangle();
+    rect.position(100, 30);
+    rect.resize(100, 40);
+    rect.attr({
+      body: {
+        fill: 'blue',
+      },
+      label: {
+        text: 'Hello',
+        fill: 'white',
+      },
+    });
+    rect.addTo(graph);
+
+    var rect2 = rect.clone() as joint.shapes.standard.Rectangle;
+    rect2.translate(300, 0);
+    rect2.attr('label/text', 'World!');
+    rect2.addTo(graph);
+
+    var link = new joint.shapes.standard.Link();
+    link.source(rect);
+    link.target(rect2);
+    link.addTo(graph);
+
+  }
 }
